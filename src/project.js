c9f7351945321a770bf664ea69af181f9126b61b
SuliatDauda <suliatdauda1@gmail.com>
2024-10-05 21:30:52 +0100

    added javascript to return current temperature. added innerHTML to temperatureBox and Math.round(temperature)

--- src/project.js
+++ src/project.js
@@ -1,9 +1,13 @@
-function updateWeather(response) {}
+function updateWeather(response) {
+  let temperatureBox = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureBox.innerHTML = Math.round(temperature);
+}
 
 function searchCity(city) {
   let apiKey = "905390deebf4616a1a1o9903d8ct5d65";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(UpdateWeather);
+  axios.get(apiUrl).then(updateWeather);
 }
 
 function handleSearchSubmit(event) {
