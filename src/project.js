65a63a06ba635a8949eba447ec2a1b71bfc39b65
SuliatDauda <suliatdauda1@gmail.com>
2024-10-05 18:44:32 +0100

    Added javascript f file, added a function handleSearchSubmit, event.preventDefault and eventListener

--- src/project.js
+++ src/project.js
@@ -1,1 +1,6 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+}
 
+let searchFormBox = document.querySelector("#search-form");
+searchFormBox.addEventListener("submit", handleSearchSubmit);
