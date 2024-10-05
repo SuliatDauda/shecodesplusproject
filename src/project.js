05894b7ebbad7830d31c003a37e1e73756e9c196
SuliatDauda <suliatdauda1@gmail.com>
2024-10-05 21:02:16 +0100

    added function searchCity

--- src/project.js
+++ src/project.js
@@ -1,8 +1,12 @@
+function searchCity(city) {}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityBox = document.querySelector("#weather-city");
   cityBox.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormBox = document.querySelector("#search-form");
