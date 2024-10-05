8bc076e386036e194bedc29901979667efc27fd4
SuliatDauda <suliatdauda1@gmail.com>
2024-10-05 21:12:34 +0100

    added apiUrl and apiKey to javascript, called axios and added function updateWeather.

--- src/project.js
+++ src/project.js
@@ -1,4 +1,10 @@
-function searchCity(city) {}
+function updateWeather(response) {}
+
+function searchCity(city) {
+  let apiKey = "905390deebf4616a1a1o9903d8ct5d65";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(UpdateWeather);
+}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
